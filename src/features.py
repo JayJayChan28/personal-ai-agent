46827edf84edf69f6dd96db73f81b5b19b85c8a9
Jayden Chan <chan.jayden@gmail.com>
2025-08-13 01:24:27 -0700

    preprocess text data by removin white space, puncuation, stopwords, and replaceing urls

--- src/features.py
+++ src/features.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import re
+import nltk
+nltk.download('stopwords')
+import string
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
 
 goemotions_df = pd.read_csv("../../personal-ai-agent-training/data/raw/goemotions.csv")
 
@@ -16,5 +23,29 @@ goemotions_df.select_dtypes(include="number").describe()
 # -------------------------------------------------------------------
 # Cleaning and preprocessing texts columns
 # -------------------------------------------------------------------
-# List of categorical columns (example: adjust as needed)
-categorical_columns = ["subreddit", "author", "parent_id", "link_id"]
+
+
+URL_RE  = re.compile(r'https?://\S+|www\.\S+')
+USER_RE = re.compile(r'@\w+')
+SUB_RE  = re.compile(r'\br/\w+')
+
+def clean_text(text):
+    text = URL_RE.sub(' <URL> ', text) #replaces URLs with <URL>
+    text = USER_RE.sub(' <USER> ', text) #replaces user mentions with <USER>
+    text = SUB_RE.sub(' <SUBREDDIT> ', text) #replaces subreddit mentions with <SUBREDDIT>
+    text = text.replace('\n', ' ') #replaces newlines with space
+    text = text.replace('\t', ' ') #replaces tabs with space
+    text = text.strip() #removes leading and trailing spaces
+    text = re.sub(r'\s+', ' ', text) #removes multiple spaces
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Remove stopwords
+    tokens = text.split()
+    tokens = [word for word in tokens if word.lower() not in stop_words]
+    text = ' '.join(tokens)
+    return text
+
+goemotions_df['text'] = goemotions_df['text'].apply(clean_text)
+
+
+text = 'dog cat fish'
+text.split()
